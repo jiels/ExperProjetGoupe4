a2d481e10eb59d6e5f8dc84f803c4838092e7904
jiels <78866769+jiels@users.noreply.github.com>
2021-02-26 18:34:21 +0100

    suppr ConnectException

--- src/Donjon/Client.java
+++ src/Donjon/Client.java
@@ -3,7 +3,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
-import java.net.ConnectException;
 import java.net.Socket;
 import java.util.Scanner;
 
